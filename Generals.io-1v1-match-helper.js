60e5adb65572d5b3cf64d61651e0116992a1cb2c
pasghetti <kenneth.moon3@yahoo.com>
2021-11-02 19:08:47 -0700

    Add time delta back

--- Generals.io-1v1-match-helper.js
+++ Generals.io-1v1-match-helper.js
@@ -414,6 +414,11 @@ function main(){
 		let FFA = "FFA";
 		let TWOVTWO = "2v2";
 		let CUSTOM = "custom";
+		let time_delta = 0;
+		let real_time = Number(new Date($.ajax({async:false}).getResponseHeader("Date")));
+		let system_time = Number(new Date());
+		time_delta = real_time - system_time;
+		console.log(time_delta);
         setInterval(async () => {
             let buttons = Array.from(document.getElementsByTagName('button')).map(a => a.innerHTML);
 			if (!buttons.find(a => a === "PLAY" || a === "1v1" || a === "Play Again" || a === "Cancel")){
@@ -518,11 +523,11 @@ function main(){
 				}
 				tasks--;
 			});
-			let time_0 = Number(new Date());
+			let time_0 = Number(new Date()) + time_delta;
 			let checkfetchendinterval = setInterval(()=>{
 				if (tasks == 0){
 					clearInterval(checkfetchendinterval);
-					let now = Number(new Date());
+					let now = Number(new Date()) + time_delta;
 					console.log(`fetch: time = ${now - time_0}`);
 					let pool = [];
 					if (enable_friends)
